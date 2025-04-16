2c4c24b3f413a40d93961f215344d8cfe6506343
Shane Loeffler <Shane98c@users.noreply.github.com>
2025-04-16 15:47:31 -0500

    rm assetPrefix

--- next.config.js
+++
@@ -1,6 +0,0 @@
-const isDev =
-  process.env.VERCEL_ENV === 'preview' || process.env.NODE_ENV === 'development'
-
-module.exports = {
-  assetPrefix: isDev ? '' : 'https://oae-efficiency.carbonplan.org',
-}
